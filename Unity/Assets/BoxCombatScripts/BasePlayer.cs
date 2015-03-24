41094af07ed6d5c925c229ef2958795a573e6058
blakeporter81 <porterb@live.unc.edu>
2015-03-23 22:50:19 -0400

    Made a crude battle state machine
    The battle state machine is found in the BoxCombat scene. The scene
    includes an arena with a blue and red box in each corner, a button that
    reads “Attack”, and text that has player and enemy health.
    When the “Attack” button is pressed, the blue box does damage to the
    red box, then the red box does damage to the blue box. The state
    machine then waits for the button to be pressed again. When the red
    box’s health reaches zero a message pops up that says “You win”.

---
+++ Unity/Assets/BoxCombatScripts/BasePlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer : MonoBehaviour {
+	public string PlayerName;
+
+	private static int health = 10;
+
+	public static int getHealth(){
+		return health;
+	}
+	/*
+	public static int getHealth{
+		get{
+			return health;
+		}
+
+	}
+	*/
+	public static int attack1 = 5;
+
+	public static int getAttack1() {
+		return attack1;
+	}
+
+	public static int attack2 = 5;
+
+	public static int getAttack2() {
+		return attack2;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/BoxCombatScripts/EnemyPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPlayer : MonoBehaviour {
+
+	public string EnemyName;
+	private static int health = 10;
+	
+	public static int getHealth(){
+			return health;
+	}
+	public static int attack1 = 3;
+	
+	public static int getAttack1() {
+		return attack1;
+	}
+	
+	public static int attack2 = 5;
+	
+	public static int getAttack2() {
+		return attack2;
+	}
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
