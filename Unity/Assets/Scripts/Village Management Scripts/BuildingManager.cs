511f7444ecf1df0fa515c7132c6be7484e4e2e50
TJ Parsons <parstj@gmail.com>
2015-04-19 14:18:21 -0400

    File did not get added in last committ

--- Unity/Assets/Scripts/Village Management Scripts/BuildingManager.cs
+++ Unity/Assets/Scripts/Village Management Scripts/BuildingManager.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class BuildingManager {
-	
-	ArrayList<Building> buildings = new ArrayList<Building>();
-
-	public void build(){
+	public const int WELL_INDEX = 1;
+	public static List<Building> buildWell(List<Building> buildings){
+		Well newWell = new Well (WELL_INDEX);
+		buildings.Add(newWell);
+		return buildings;
 	}
 }
