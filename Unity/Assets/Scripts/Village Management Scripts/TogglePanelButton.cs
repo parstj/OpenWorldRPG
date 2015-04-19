a4d6d86d1751476c32646c9b4958d750b7adae0f
TJ Parsons <parstj@gmail.com>
2015-04-19 18:21:16 -0400

    Added Building panel toggle

---
+++ Unity/Assets/Scripts/Village Management Scripts/TogglePanelButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePanelButton : MonoBehaviour {
+
+	public void TogglePanel (GameObject panel) {
+		panel.SetActive (!panel.activeSelf);
+	}
+}
