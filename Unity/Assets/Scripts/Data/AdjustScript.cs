90d6963c09d8e3ff8b581e462dfae0c098f31c21
blacktheon <blacktheon@gmail.com>
2015-04-20 13:18:28 -0400

    data control

---
+++ Unity/Assets/Scripts/Data/AdjustScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustScript : MonoBehaviour {
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (10, 100, 150, 30), "Health up")) {
+			GameControl.control.health += 10;
+		}
+		if (GUI.Button (new Rect (10, 140, 150, 30), "Health down")) {
+			GameControl.control.health -= 10;
+		}
+		if (GUI.Button (new Rect (10, 180, 150, 30), "Experience up")) {
+			GameControl.control.experience += 10;
+		}
+		if (GUI.Button (new Rect (10, 220, 150, 30), "Experience down")) {
+			GameControl.control.experience -= 10;
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Data/GameControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl control;
+
+	public float health;
+	public float experience;
+	
+	void Awake () {
+		if (control == null) {
+			DontDestroyOnLoad (gameObject);
+			control = this;
+		} else if(control != this){
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnGUI(){
+		GUI.Label (new Rect(10, 10, 150, 30), "Health: " + health);
+		GUI.Label (new Rect(10, 40, 150, 30), "Experience: " + experience);
+	}
+}
