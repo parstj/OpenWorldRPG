0555e9aa6d22aa379dd7c34e25309b171c62993a
blacktheon <blacktheon@gmail.com>
2015-04-30 06:28:16 -0400

    Fixed bug: after coming conversation with friend NPC, player behaves wired. every time it jumps, it go back to the FriendExit point.

--- Unity/Assets/Scripts/test1 scripts/FriendControl.cs
+++ Unity/Assets/Scripts/test1 scripts/FriendControl.cs
@@ -5,10 +5,16 @@ public class FriendControl : MonoBehaviour {
 	public GameObject camera1;
 	public GameObject camera2;
 
+	public GameObject panel1;
+	public GameObject panel2;
+	public GameObject panel3;
+
 	public GameObject returnPoint;
 	public GameObject player;
 
 	public void Accept(){
+		panel1.SetActive (false);
+
 		GameControl.control.openWorldData.mission1.started = true;
 		player.transform.position = returnPoint.transform.position;
 		camera1.SetActive (true);
@@ -16,6 +22,10 @@ public class FriendControl : MonoBehaviour {
 	}
 
 	public void Refuse(){
+		panel1.SetActive (false);
+		panel2.SetActive (false);
+		panel3.SetActive (false);
+
 		player.transform.position = returnPoint.transform.position;
 		camera1.SetActive (true);
 		camera2.SetActive (false);
